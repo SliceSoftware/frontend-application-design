85b74ff916a68a29eecddc611ee5bba5eaa32a58
Joshua Shapiro <joshua.shapiro@eighteenfiftynine.com>
2023-01-07 03:47:27 -0800

    Added myUseState Hook

--- todo-list-project/src/TodoApp.js
+++ todo-list-project/src/TodoApp.js
@@ -1,27 +1,26 @@
 
 import { TodoList } from './TodoList';
 import { TodoListItemCreator } from './TodoListItemCreator';
-import { VDOM } from './VirtualDom'
+import { myUseState } from './VirtualDom'
 import './TodoApp.css';
 import v from './v';
 
-const todoItems = [
-    {complete: true, text: "This is complete"},
-    {complete: false, text: "This is not complete"},
-];
-
-const addItem = (text) => {
-    const item = {complete: false, text};
-    todoItems.push(item);
-    VDOM.refresh();
-}
-
-const updateItem = (i, item) => {
-    todoItems[i] = item;
-    VDOM.refresh();
-}
 
 function TodoApp() {
+
+    const [todoItems, setTodoItems] = myUseState([]);
+
+    const addItem = (text) => {
+        const item = {complete: false, text};
+        todoItems.push(item);
+        setTodoItems(todoItems);
+    }
+
+    const updateItem = (i, item) => {
+        todoItems[i] = item;
+        setTodoItems(todoItems);
+    }
+
     const app = v('div', {className: 'TodoApp'});
     app.add(TodoListItemCreator(addItem));
     app.add(TodoList(todoItems, updateItem));
--- todo-list-project/src/VirtualDom.js
+++ todo-list-project/src/VirtualDom.js
@@ -20,8 +20,49 @@ class VirtualDom {
         if (this.mountpoint) {
             const new_element = this.root();
             this.mountpoint.replaceChildren(new_element);
+            resetHookIndicies();
         }
     }
 }
 
 export const VDOM = new VirtualDom();
+
+// Cache state pairs in this array
+const hooks = [];
+
+// Reset this to zero at the end of a rendering cycle
+// Increment it during each invocation of myUseState
+let hookIndex = 0;
+
+/**
+ * Reset indicies for all hooks cached in the VDOM
+ */
+function resetHookIndicies() {
+    hookIndex = 0;
+}
+
+/**
+ * Return a two element array.  
+ * First element is the current value 
+ * Second element is a function that sets the value.
+ * 
+ * @param initial  The initial value
+ */
+export function myUseState(initial) { 
+  
+  // Return the cached pair if it exists
+  let pair = hooks[hookIndex];
+  if (pair) {
+      hookIndex++;
+      return pair;
+  }
+
+  // No cached pair found. Initialize a new one.
+  pair = [initial, (v) => { pair[0] = v; VDOM.refresh(); }];
+
+  // Cache the pair.
+  hooks[hookIndex++] = pair;
+  
+  return pair;
+
+}
