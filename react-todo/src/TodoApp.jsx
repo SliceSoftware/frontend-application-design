d3846c146a88e11840b2fbb4079f6b6aa4b6f2b6
Joshua Shapiro <joshua.shapiro@eighteenfiftynine.com>
2023-01-07 06:06:20 -0800

    Chapter 4 solution.  Basic React App

---
+++ react-todo/src/TodoApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainLayout from './layout/MainLayout';
+import TodoListView from './views/TodoListView';
+
+const TodoApp = () => {
+    return (<MainLayout>
+        <TodoListView list={{name: "Grocery List"}}/>
+    </MainLayout>)
+}
+
+export default TodoApp;
