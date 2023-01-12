ff3e150b5b73814ade21142bfc7a91fa5e811c31
Joshua Shapiro <joshua.shapiro@eighteenfiftynine.com>
2023-01-12 09:28:05 -0800

    Merged main

--- merge-main.sh
+++ merge-main.sh
@@ -2,6 +2,7 @@
 for b in $( git branch --list "chapter*" ); do
     echo "Merging main into $b"
     git checkout $b
+    git pull --rebase
     git merge --squash main 
     git commit -am "Merged main"
 done
